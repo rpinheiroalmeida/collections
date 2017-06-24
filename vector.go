3352267cee2a0581d72422c136701b4c6ad6ae46
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2017-06-24 18:16:33 -0300

    Adding vector and matrix to collections package

---
+++ vector.go
@@ -0,0 +1,94 @@
+package collections
+
+import (
+	"math"
+)
+
+type Vector []float64
+type binaryCondition func(v1, v2 float64) float64
+
+func validate(v Vector) {
+	if len(v) == 0 {
+		panic("empty sample supplyed")
+	}
+}
+
+func (v Vector) Len() int {
+	return len(v)
+}
+
+func (v Vector) Less(i, j int) bool {
+	return v[i] < v[j]
+}
+
+func (v Vector) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
+
+//Add subtracts two vectors element wise
+func (v Vector) Add(w Vector) Vector {
+	tuples := Zip(v, w)
+
+	result := make(Vector, len(tuples))
+
+	for i, value := range tuples {
+		result[i] = value.A + value.B
+	}
+	return result
+}
+
+//Subtract subtracts two vectors elementwise
+func (v Vector) Subtract(w Vector) Vector {
+	tuples := Zip(v, w)
+
+	result := make(Vector, len(tuples))
+
+	for i, value := range tuples {
+		result[i] = value.A - value.B
+	}
+	return result
+}
+
+func (v Vector) Dot(w Vector) float64 {
+	tuples := Zip(v, w)
+	var sum float64
+	for _, tuple := range tuples {
+		sum += tuple.A * tuple.B
+	}
+	return sum
+}
+
+func (v Vector) SumOfSquares() float64 {
+	return v.Dot(v)
+}
+
+func (v Vector) SquaredDistance(w Vector) float64 {
+	result := v.Subtract(w)
+	return result.SumOfSquares()
+}
+
+func (v Vector) Distance(w Vector) float64 {
+	return math.Sqrt(v.SquaredDistance(w))
+}
+
+func matchingValue(fn binaryCondition, initial float64, vector Vector) float64 {
+	validate(vector)
+
+	current := initial
+	for _, value := range vector {
+		current = fn(current, value)
+	}
+	return current
+}
+
+func (v Vector) Max() float64 {
+	return matchingValue(math.Max, math.Inf(-1), v)
+}
+
+func (v Vector) Min() float64 {
+	return matchingValue(math.Min, math.Inf(+1), v)
+}
+
+func (v Vector) Empty() bool {
+	return v.Len() == 0
+}
